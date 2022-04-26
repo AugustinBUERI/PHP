4cb1ff1037c5a32d487d2109599b446c83d53eb1
AugustinBUERI <augustin.bueri@epitech.eu>
2022-04-26 17:23:36 +0200

    feat entities

---
+++ symfony-rest-api-noapip/src/Entity/Order.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrderRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=OrderRepository::class)
+ * @ORM\Table(name="`order`")
+ */
+class Order
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $totalPrice;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $creationdate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTotalPrice(): ?float
+    {
+        return $this->totalPrice;
+    }
+
+    public function setTotalPrice(float $totalPrice): self
+    {
+        $this->totalPrice = $totalPrice;
+
+        return $this;
+    }
+
+    public function getCreationdate(): ?\DateTimeInterface
+    {
+        return $this->creationdate;
+    }
+
+    public function setCreationdate(\DateTimeInterface $creationdate): self
+    {
+        $this->creationdate = $creationdate;
+
+        return $this;
+    }
+}
